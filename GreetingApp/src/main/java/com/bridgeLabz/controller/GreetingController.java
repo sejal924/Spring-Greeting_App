e7afc2ade0c8e542d435a84be2f7f804e7a597e1
sejal924 <sejalchouhan24@gmail.com>
2025-02-25 10:39:08 +0530

    [Sejal]Add.Ability for the greeting app to find the greeting message

--- GreetingApp/src/main/java/com/bridgeLabz/controller/GreetingController.java
+++ GreetingApp/src/main/java/com/bridgeLabz/controller/GreetingController.java
@@ -1,8 +1,11 @@
 package com.bridgeLabz.controller;
 
+import com.bridgeLabz.model.Greeting;
 import com.bridgeLabz.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class GreetingController {
@@ -11,29 +14,40 @@ public class GreetingController {
     private GreetingService greetingService;
 
     @GetMapping("/greeting")
-    public String getGreeting(){
+    public String getGreeting() {
         return "GET : " + greetingService.getGreeting();
     }
 
     @PostMapping("/greeting")
-    public String postGreeting(){
+    public String postGreeting() {
         return "POST : " + greetingService.getGreeting();
     }
 
     @PutMapping("/greeting")
-    public String putGreeting(){
+    public String putGreeting() {
         return "PUT : " + greetingService.getGreeting();
     }
 
     @DeleteMapping("/greeting")
-    public String deleteGreeting(){
+    public String deleteGreeting() {
         return "DELETE : " + greetingService.getGreeting();
     }
 
-    //UseCase: 3
+    //UseCase: 4
     @GetMapping("/greeting/params")
-    public String getGreetingParams(@RequestParam(value = "firstName",required = false) String firstName,
-                                    @RequestParam(value = "lastName",required = false) String lastName){
-        return greetingService.getGreeting(firstName,lastName);
+    public String getGreetingParams(@RequestParam(value = "firstName", required = false) String firstName,
+                                    @RequestParam(value = "lastName", required = false) String lastName) {
+        return greetingService.getGreeting(firstName, lastName);
+    }
+
+    //UseCase: 5
+    @GetMapping("/greeting/{id}")
+    public String getGreetingById(@PathVariable long id) {
+        Optional<Greeting> greeting = greetingService.findGreetingById(id);
+        if (greeting.isPresent()) {
+            return greeting.get().getMessage();
+        } else {
+            return "Greeting not found";
+        }
     }
 }
--- GreetingApp/src/main/java/com/bridgeLabz/service/GreetingService.java
+++ GreetingApp/src/main/java/com/bridgeLabz/service/GreetingService.java
@@ -5,6 +5,8 @@ import com.bridgeLabz.repository.GreetingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class GreetingService {
     public String getGreeting(){
@@ -32,5 +34,9 @@ public class GreetingService {
     public void saveGreeting(String message) {
         Greeting greeting = new Greeting(message);
         greetingRepository.save(greeting);
+    }
+
+    public Optional<Greeting> findGreetingById(long id){
+        return greetingRepository.findById(id);
     }
 }
